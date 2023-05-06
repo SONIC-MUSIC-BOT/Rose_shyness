95807d67d9405e14579dd5b816a4fd42de770f19
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-05-07 02:45:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/b5281f70c4dc630
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/56d1760224589ee370186.jpg",
+    "https://telegra.ph/file/8e17df454c4b1d206fdc7.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
