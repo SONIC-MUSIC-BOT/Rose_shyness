290916460d044f57a783680d0d25334d6a343fae
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-05-07 20:47:30 +0300

    Update settings.py

--- AnonX/plugins/settings.py
+++ AnonX/plugins/settings.py
@@ -6,6 +6,7 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
 from config import (BANNED_USERS, CLEANMODE_DELETE_MINS,
                     MUSIC_BOT_NAME, OWNER_ID)
 from strings import get_command
+from strings.filters import command
 from AnonX import app
 from AnonX.utils.database import (add_nonadmin_chat,
                                        cleanmode_off, cleanmode_on,
@@ -36,6 +37,12 @@ SETTINGS_COMMAND = get_command("SETTINGS_COMMAND")
 
 @app.on_message(
     filters.command(SETTINGS_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@app.on_message(
+    command(["اعدادات"])
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
