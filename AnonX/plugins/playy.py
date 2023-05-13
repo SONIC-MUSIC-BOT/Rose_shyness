14ba107c045623daece8b6dba5f8a67f8807aa64
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-05-13 17:31:51 +0300

    Update playy.py

--- AnonX/plugins/playy.py
+++ AnonX/plugins/playy.py
@@ -41,7 +41,7 @@ from AnonX.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 @app.on_message(
-    command(["قناه","قناة","تشغيل-قناه","تشغيل-قناة"])
+    command(["شغل","قناة","تشغيل-قناه","تشغيل-قناة"])
     & filters.channel
     & ~filters.edited
     & ~BANNED_USERS
