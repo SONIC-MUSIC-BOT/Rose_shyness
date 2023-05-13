f979a8a47098164addebff871c8eb0ebd2a0fcdf
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-05-13 16:37:09 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -3,7 +3,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import config
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP, YAFA_CHANNEL, YAFA_NAME
+from config import  SUPPORT_CHANNEL, SUPPORT_GROUP, YAFA_CHANNEL, YAFA_NAME
 from AnonX import app
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
@@ -71,24 +71,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ]
     )
-    if GITHUB_REPO and OWNER:
+    if  OWNER:
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
                 ),
             ]
         )
-    else:
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                    ),
-                ]
-            )
+    else: 
         if OWNER:
             buttons.append(
                 [
