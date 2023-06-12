6c7afb45acabbcb3e7b647f3bd997f600f5e0380
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-06-12 21:22:39 +0300

    Add files via upload

---
+++ AnonX/plugins/start & end video chat.py
@@ -0,0 +1,56 @@
+
+
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, VoiceChatStarted, VoiceChatEnded
+from AnonX import app
+
+@app.on_message(filters.voice_chat_started)
+async def brah(client, message):
+       await message.reply("تم بدأ محادثة صوتية جديدة.")
+
+@app.on_message(filters.voice_chat_ended)
+async def brah2(client, message):
+    da = message.voice_chat_ended.duration
+    ma = divmod(da, 60)
+    ho = divmod(ma[0], 60)
+    day = divmod(ho[0], 24)
+    if da < 60:
+       await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {da} ثواني**")        
+    elif 60 < da < 3600:
+        if 1 <= ma[0] < 2:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها دقيقه**")
+        elif 2 <= ma[0] < 3:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها دقيقتين**")
+        elif 3 <= ma[0] < 11:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {ma[0]} دقايق**")  
+        else:
+            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {ma[0]} دقيقه**") 
+    elif 3600 < da < 86400:
+        if 1 <= ho[0] < 2:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها ساعه**")
+        elif 2 <= ho[0] < 3:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها ساعتين**")
+        elif 3 <= ho[0] < 11:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {ho[0]} ساعات**")  
+        else:
+            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {ho[0]} ساعة**")
+    else:
+        if 1 <= day[0] < 2:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها يوم**")
+        elif 2 <= day[0] < 3:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها يومين**")
+        elif 3 <= day[0] < 11:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {day[0]} ايام**")  
+        else:
+            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {day[0]} يوم**")
+
+async def send_invite(message):
+   for x in message.voice_chat_members_invited.users:
+       try:
+        link = await app.export_chat_invite_link(message.chat.id)
+       except:
+        link = "البوت ليس ادمن لم استطع انشاء رابط"
+       await app.send_message(x.id, f"قام هذا الشخص {message.from_user.mention} \n بدعوتك الي المحادثة الصوتية \nرابط المجموعة : {link}")
